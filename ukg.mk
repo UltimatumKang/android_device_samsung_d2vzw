dee1cbb259a7f66184d1ea1da28dc50810387229
David Hou <david.hou314@gmail.com>
2013-05-18 20:51:10 -0700

    This seems to work

--- ukg.mk
+++ ukg.mk
@@ -1,17 +1,17 @@
-# Inherit AOSP device configuration for d2spr
+# Inherit ukg device configuration for d2spr
 $(call inherit-product, device/samsung/d2vzw/full_d2vzw.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/ukg/config/common.mk)
 
 # Inherit CDMA common stuff
-$(call inherit-product, vendor/aosp/config/cdma.mk)
+$(call inherit-product, vendor/ukg/config/cdma.mk)
 
 # d2spr Overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/toro
+PRODUCT_PACKAGE_OVERLAYS += vendor/ukg/overlay/toro
 
 # Setup device specific product configuration.
-PRODUCT_NAME := xylon_d2vzw
+PRODUCT_NAME := ukg_d2vzw
 PRODUCT_BRAND := Samsung
 PRODUCT_DEVICE := d2vzw
 PRODUCT_MANUFACTURER := Samsung
@@ -20,7 +20,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FIN
 
 # Copy mako specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/aosp/prebuilt/hybrid_xhdpi_no_nav.conf:system/etc/beerbong/properties.conf \
-    vendor/aosp/prebuilt/bootanimation/bootanimation_1280_720.zip:system/media/bootanimation.zip \
-    vendor/aosp/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/aosp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd 
+    vendor/ukg/prebuilt/hybrid_xhdpi_no_nav.conf:system/etc/beerbong/properties.conf \
+    vendor/ukg/prebuilt/bootanimation/bootanimation_1280_720.zip:system/media/bootanimation.zip \
+    vendor/ukg/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/ukg/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd 
